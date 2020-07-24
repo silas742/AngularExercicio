806161f7434f0e403fbc272e9a1fc7b08db1d725
Silas <silas898esperto@gmail.com>
2020-07-24 13:02:57 -0300

    Exercicio Angular

---
+++ src/app/Exercicio/exercicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'; @Component({
+    selector:"Exercicio-Angular",
+    templateUrl:'./exercicio.component.html',
+    styleUrls:['./exercicio.component.css']
+})
+
+
+
+export class ExercicioComponent { 
+    framework="Angular";
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExercicioComponent } from './Exercicio/exercicio.component';
+import { OlaAngularCliComponent } from './ola-angular-cli/ola-angular-cli.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExercicioComponent,
+    OlaAngularCliComponent
   ],
   imports: [
     BrowserModule
